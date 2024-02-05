329ed95ee882298c17688b1fabeede655902dea6
Волков Виктор Владимирович <10231351@college.local>
2024-02-05 18:58:37 +0300

    other: add task_04

---
+++ src/class_2024-02-05/Task_04/Program.cs
@@ -0,0 +1,35 @@
+﻿/* Задан одномерный массив А[1..20].
+ * Найти минимальный элемент среди элементов массива с n-го по k-й (n и k вводятся с клавиатуры)
+ */
+
+namespace Task_04
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Random random = new Random();
+            int[] array = new int[20];
+            for (int i = 0; i < 20; i++)
+            {
+                array[i] = random.Next(-100, 100);
+            }
+
+            Console.Write("Enter first array index: ");
+            int n = int.Parse(Console.ReadLine());
+            Console.Write("Enter last array index: ");
+            int k = int.Parse(Console.ReadLine());
+
+            int min = array[n];
+
+            for (int i = n; i < k+1; i++)
+            {
+                if (array[i] < min) min = array[i];
+            }
+
+            // Output
+            Console.WriteLine("Source array items: " + String.Join(' ', array));
+            Console.WriteLine($"Min value from range between indices {n} and {k}: {min}");
+        }
+    }
+}
