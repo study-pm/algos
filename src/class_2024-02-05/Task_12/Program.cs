c0cbad3408cc12506ea1d7fb1fff6c9da67579f4
Волков Виктор Владимирович <10231351@college.local>
2024-02-05 20:59:26 +0300

    chore: add task_12

---
+++ src/class_2024-02-05/Task_12/Program.cs
@@ -0,0 +1,14 @@
+﻿/*  В одномерном массиве целых чисел a1, а2, ..., an найти номер первого четного числа. 
+ *  Если четных чисел нет, то ответом может быть число 0
+ */
+
+namespace Task_12
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
