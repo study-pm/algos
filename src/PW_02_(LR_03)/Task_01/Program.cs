ad7578f04054ff52e86553e0b1bcec765043239a
viktor <sterilework@gmail.com>
2024-02-05 15:35:09 +0300

    other: add code for pw_02

---
+++ src/PW_02_(LR_03)/Task_01/Program.cs
@@ -0,0 +1,61 @@
+﻿/* Написать программу, вычисляющую значение функции у:
+ * – если х < -5, то у(х) = а*х2;
+ * – если -5 ≤ х < 1, то у(х) = а*|х|;
+ * – если х ≥ 1, то у(х) = 1/(а-х).
+ *
+ * Числа а и х вводятся пользователем.
+ * Результат округлять до трех знаков после запятой.
+ */
+
+namespace Task_01
+{
+    internal class Program
+    {
+        static double? f (double a, double x)
+        {
+            if (x < -5)
+            {
+                return a * Math.Pow(x, 2);
+            }
+            else if (x < 1 && x >= -5)
+            {
+                return a * Math.Abs(x);
+            }
+            else if (a != x)
+            {
+                return 1 / (a - x);
+            }
+            return null;
+        }
+        static void Main(string[] args)
+        {
+            try
+            {
+                // User input
+                Console.Write("Enter param a (must be a valid real number): ");
+                double a = double.Parse(Console.ReadLine());
+                Console.Write("Enter variable x (must be a valid real number): ");
+                double x = Convert.ToDouble(Console.ReadLine());
+
+                // Calculation
+                double? y = f(a, x);
+
+                // Output
+                if (y != null)
+                {
+                    Console.WriteLine("y = " + y?.ToString("N3"));
+                }
+                else
+                {
+                    Console.WriteLine("No solution: division by zero, quotient is undefined");
+                }
+            }
+            catch (Exception)
+            {
+                // Handle invalid decimal separator
+                string decimalSeparator = System.Globalization.NumberFormatInfo.CurrentInfo.NumberDecimalSeparator;
+                Console.WriteLine("Incorrect input: use decimal separator according to your system current culture settings ({0})", decimalSeparator);
+            }
+        }
+    }
+}
---
+++ src/PW_02_(LR_03)/Task_03/Program.cs
@@ -0,0 +1,71 @@
+﻿/* Написать программу, находящую площадь треугольника, если его можно построить по введенным пользователем трем числам (длинам сторон). 
+ * Определить вид треугольника.
+ *
+ * Для вычисления площади S использовать формулу Герона:
+ * S=√(p*(p-a)*(p-b)*(p-c)),
+ * где р — полупериметр.
+ *
+ * Результат округлять до двух знаков после запятой.
+ * Если треугольник построить нельзя, сообщить об этом пользователю.
+ */
+
+namespace Task_03
+{
+    internal class Program
+    {
+        static bool CheckIfTriangle(double[] sides)
+        {
+            if (sides[0] + sides[1] > sides[2] && sides[1] + sides[2] > sides[0] && sides[2] + sides[0] > sides[1]) return true;
+            return false;
+        }
+        static double GetTriangleSquare(double[] sides)
+        {
+            double p = sides.Sum() / 2;
+            return Math.Sqrt(p * (p - sides[0]) * (p - sides[1]) * (p - sides[2]));
+        }
+        static string GetTriangleType(double[] sides)
+        {
+            if (
+                Math.Pow(sides[0], 2) > Math.Pow(sides[1], 2) + Math.Pow(sides[2], 2) ||
+                Math.Pow(sides[1], 2) > Math.Pow(sides[0], 2) + Math.Pow(sides[2], 2) ||
+                Math.Pow(sides[2], 2) > Math.Pow(sides[0], 2) + Math.Pow(sides[1], 2)
+                )
+
+            {
+                return "obtuse";
+            }
+            else if (
+                Math.Pow(sides[0], 2) < Math.Pow(sides[1], 2) + Math.Pow(sides[2], 2) ||
+                Math.Pow(sides[1], 2) < Math.Pow(sides[0], 2) + Math.Pow(sides[2], 2) ||
+                Math.Pow(sides[2], 2) < Math.Pow(sides[0], 2) + Math.Pow(sides[1], 2)
+                )
+            {
+                return "acute";
+            }
+            return "right";
+        }
+        static void Main(string[] args)
+        {
+            // User input
+            Console.Write("Enter triangle sides, separated by an ordinary space: ");
+            string[] inputArr = Console.ReadLine().Split(' ');
+            double[] sides = new double[inputArr.Length];
+            for (int i = 0; i < inputArr.Length; i++) sides[i] = double.Parse(inputArr[i]);
+
+            // Check if triangle exists
+            if (!CheckIfTriangle(sides))
+            {
+                Console.WriteLine("Input sides does not make a triangle");
+                return;
+            }
+
+            // Calculation
+            double S = GetTriangleSquare(sides);
+            string t = GetTriangleType(sides);
+
+            // Output
+            Console.WriteLine($"Triangle square is {S.ToString("N2")}. Triangle is {t}");
+
+        }
+    }
+}
