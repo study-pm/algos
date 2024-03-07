9fcea4cf9aaf17ab48b79db0267cb24f46a7fcdb
viktor <sterilework@gmail.com>
2024-03-07 18:49:44 +0300

    fix: add parse as natural method, error handling

--- src/class_2024-02-05/Task_04/Program.cs
+++ src/class_2024-02-05/Task_04/Program.cs
@@ -6,6 +6,33 @@ namespace Task_04
 {
     internal class Program
     {
+        /// <summary>
+        /// Parse a string and convert to a natural number
+        /// </summary>
+        /// <param name="s">string representing a natural number</param>
+        /// <returns>Parsed number</returns>
+        /// <exception cref="Exception">Not an integer or natural number</exception>
+        public static int ParseAsNatural(string s)
+        {
+            int n = int.Parse(s);
+            if (n < 1) throw new Exception("not a natural number");
+            return n;
+        }
+        /// <summary>
+        /// Check and fix item range
+        /// </summary>
+        /// <param name="k">First element number</param>
+        /// <param name="n">Last element number</param>
+        /// <returns>Fixed element numbers</returns>
+        /// <exception cref="Exception"></exception>
+        public static (int, int) CheckItemRange(int n, int k)
+        {
+            if (k > 20 || n > 20) throw new Exception("outside the item range");
+            int first = n;
+            int last = k;
+            if (last < first) (first, last) = (last, first);
+            return (first, last);
+        }
         static void Main(string[] args)
         {
             try
@@ -16,28 +43,27 @@ namespace Task_04
                 {
                     A[i] = random.Next(-100, 100);
                 }
+                Console.Write("Enter first element number (must be an integer from 1 to 20): ");
+                int n = ParseAsNatural(Console.ReadLine());
+                Console.Write("Enter last element number (must be an integer from 1 to 20): ");
+                int k = ParseAsNatural(Console.ReadLine());
 
-                Console.Write("Enter first array index: ");
-                int n = int.Parse(Console.ReadLine());
-                Console.Write("Enter last array index: ");
-                int k = int.Parse(Console.ReadLine());
+                (n, k) = CheckItemRange(n, k);
 
-                if (k < n) (n, k) = (k, n);
+                int min = A[n-1];
 
-                int min = A[n];
-
-                for (int i = n-1; i < k; i++)
+                for (int i = n; i < k; i++)
                 {
                     if (A[i] < min) min = A[i];
                 }
 
                 // Output
                 Console.WriteLine("Source array items: " + String.Join(' ', A));
-                Console.WriteLine($"Min value from range between indices {n} and {k}: {min}");
+                Console.WriteLine($"Min value from range between element numbers from {n} to {k}: {min}");
             }
-            catch (Exception)
+            catch (Exception e)
             {
-                Console.WriteLine("Invalid input");
+                Console.WriteLine("Invalid input: " + e.Message);
             }
         }
     }
