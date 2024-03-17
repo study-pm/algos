89a3d3091b713d35adb95b2150805803a9c34cd1
viktor <sterilework@gmail.com>
2024-03-17 11:54:51 +0300

    feat: add student class

---
+++ src/PW_OOP9_10/Program/Student.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Xml.Linq;
+
+namespace Program
+{
+    public class Student : Person
+    {
+        public Person PersonalData { get; set; }
+        public string ContactData { get; set; }
+        public DateTime EdStartDate { get; set; }
+
+        public Student(Person personalData, string contactData, DateTime edStartDate)
+        {
+            PersonalData = personalData;
+            ContactData = contactData;
+            EdStartDate = edStartDate;
+        }
+        public Student()
+        {
+            PersonalData = new Person();
+            ContactData = "n/a";
+            EdStartDate = DateTime.Now;
+        }
+        public override string ToString()
+        {
+            return base.ToString() + Environment.NewLine + String.Format("Contact data: " + ContactData + "\nEducation start date: " + EdStartDate.ToShortDateString());
+        }
+    }
+}
