9c10dd2e98ec15169c94086b90ca65f0a0fb5229
viktor <sterilework@gmail.com>
2024-03-24 11:18:57 +0300

    feat: add and test person class

---
+++ src/tasks_inheritance/Task/Program.cs
@@ -0,0 +1,25 @@
+﻿namespace Task
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Person person;
+            try
+            {
+                Console.Write("Enter person's first name, last name and date of birth separated by spaces: ");
+                string[] input = Console.ReadLine().Split();
+                if (input.Length != 3) throw new Exception("Incomplete input - invalid format, some data is missing.");
+                person = new Person(input[0], input[1], DateTime.Parse(input[2]));
+            }
+            catch(Exception e)
+            {
+                Console.WriteLine($"Invalid input: {e.Message}");
+                Console.WriteLine("Default person object will be crated.");
+                person = new Person();
+            }
+            Console.WriteLine("New person item is created.");
+            Console.WriteLine(person.ToString());
+        }
+    }
+}
