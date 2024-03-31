20ee7e65ce817609c651afe6d079ba68fe59c76a
viktor <sterilework@gmail.com>
2024-03-31 16:11:42 +0300

    feat: create new players

--- src/tasks_inheritance/Task/Program.cs
+++ src/tasks_inheritance/Task/Program.cs
@@ -4,22 +4,40 @@
     {
         static void Main(string[] args)
         {
-            Person person;
+            string title1 = "Creating default player";
+            Console.WriteLine(title1);
+            Console.WriteLine(new string('*', title1.Length));
+            Person player1 = new Player();
+            Console.WriteLine("Player properties.");
+            Console.WriteLine(player1.ToString());
+
+            Console.WriteLine(Environment.NewLine);
+
+            string title2 = "Creating player with user params input";
+            Console.WriteLine(title2);
+            Console.WriteLine(new string('*', title2.Length));
+            Person player2;
             try
             {
-                Console.Write("Enter person's first name, last name and date of birth separated by spaces: ");
-                string[] input = Console.ReadLine().Split();
-                if (input.Length != 3) throw new Exception("Incomplete input - invalid format, some data is missing.");
-                person = new Person(input[0], input[1], DateTime.Parse(input[2]));
+                Console.WriteLine("Enter player's data, one property per line. Press <Enter> when done with each prop.");
+                string[] props = { "First name", "Second name", "Birth date", "Position in team", "Team joining date" };
+                dynamic[] input = new dynamic[5];
+                for (int i = 0; i < props.Length; i++)
+                {
+                    Console.Write(props[i] + ": ");
+                    input[i] = (i == 2 || i == 4) ? DateTime.Parse(Console.ReadLine()) : Console.ReadLine();
+                }
+                player2 = new Player(input[0], input[1], input[2], input[3], input[4]);
             }
-            catch(Exception e)
+            catch (Exception e)
             {
                 Console.WriteLine($"Invalid input: {e.Message}");
-                Console.WriteLine("Default person object will be crated.");
-                person = new Person();
+                Console.WriteLine("Default player instance will be crated.");
+                player2 = new Person();
             }
-            Console.WriteLine("New person item is created.");
-            Console.WriteLine(person.ToString());
+
+            Console.WriteLine("Player properties.");
+            Console.WriteLine(player2.ToString());
         }
     }
 }
