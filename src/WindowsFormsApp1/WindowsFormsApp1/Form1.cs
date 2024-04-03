5e84a290dd026db49b42fc4c6808a784dd310f9e
Волков Виктор Владимирович <10231351@college.local>
2024-04-03 19:20:20 +0300

    feat: add sample windows forms app

---
+++ src/WindowsFormsApp1/WindowsFormsApp1/Form1.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using WindowsFormsApp1.Properties;
+
+namespace WindowsFormsApp1
+{
+    public partial class FormMain : Form
+    {
+        public FormMain()
+        {
+            InitializeComponent();
+        }
+
+        private void MainForm_Click(object sender, EventArgs e)
+        {
+            // ButtonMain.Text = TextBoxMain.Text;
+            TextBoxMain.Text = "Button clicked";
+            ButtonMain.BackgroundImage = Resources.traffic;
+            PictureBoxMain.Image = Resources.images;
+        }
+
+        private void TextBoxMain_TextChanged(object sender, EventArgs e)
+        {
+            ButtonMain.Text = TextBoxMain.Text;
+        }
+
+        private void radioButton1_CheckedChanged(object sender, EventArgs e)
+        {
+            if (radioButton1.Checked)
+            {
+                TextBoxMain.Text = "First option";
+            }
+            else if (radioButton2.Checked)
+            {
+                TextBoxMain.Text = "Second option";
+            }
+        }
+    }
+}
