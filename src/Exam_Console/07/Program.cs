3827d7e755fe43eb8fb6ef5a308de48750548d01
viktor <sterilework@gmail.com>
2024-06-18 18:07:20 +0300

    feat: add project solution for problem 7

---
+++ src/Exam_Console/07/Program.cs
@@ -0,0 +1,23 @@
+﻿/* Идет k-я секунда суток.
+ * Определить, сколько целых часов (Н) и целых минут (М) прошло с начала суток.
+ */
+
+namespace Console_07
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            DateTime now = DateTime.Now;
+            int k = (int)(now - now.Date).TotalSeconds;
+            // int k = (int)now.Subtract(now.Date).TotalSeconds;
+
+            Console.WriteLine(k);
+
+            int H = k / 60 / 60;
+            int M = k % 3600 / 60;
+
+            Console.WriteLine("{0} hour and {1} minutes elapsed from the beginning of the day", H, M);
+        }
+    }
+}
