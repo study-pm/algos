1e0410fde2619794490a30c59c6db2de8866c75f
viktor <sterilework@gmail.com>
2024-06-19 21:59:45 +0300

    feat: add project solution for problem 11

---
+++ src/Exam_Console/11/Program.cs
@@ -0,0 +1,39 @@
+﻿/* Даны целые числа А, В.
+ * Если числа не равны, то заменить каждое из них одним и тем же числом, равным большему из исходных,
+ * а если равны, то заменить числа нулями.
+ */
+
+namespace Console_11
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string msg = "Enter integer number";
+            try
+            {
+                Console.Write(msg + " A: ");
+                int A = int.Parse(Console.ReadLine());
+
+                Console.Write(msg + " B: ");
+                int B = int.Parse(Console.ReadLine());
+
+                if (A == B)
+                {
+                    A = B = 0;
+                }
+                else
+                {
+                    int max = Math.Max(A, B);
+                    int maximum = A > B ? A : B;
+                    A = B = max;
+                }
+                Console.WriteLine($"A = {A}, B = {B}");
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine("Invalid input: " + e.Message);
+            }
+        }
+    }
+}
