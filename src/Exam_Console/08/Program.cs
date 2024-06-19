72d2a2545111a62cc4dc91d5d43440eb2de6f8a8
viktor <sterilework@gmail.com>
2024-06-19 11:14:08 +0300

    feat: add project solution for problem 8

---
+++ src/Exam_Console/08/Program.cs
@@ -0,0 +1,31 @@
+﻿/* Определить площадь заштрихованной фигуры (рисунок 1), если известна длина стороны квадрата. */
+
+namespace Console_08
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            try
+            {
+                Console.Write("Enter a: ");
+                double a = double.Parse(Console.ReadLine());
+
+                if (a < 0) throw new Exception("Must be a non-negative number");
+
+                double s_outerSquare = a * a;
+                double s_innerSquare = Math.Pow(a - a / 5 * 2, 2);
+                double s_triangle = s_innerSquare / 2;
+                double s_rim = s_outerSquare - s_innerSquare;
+
+                double s = s_rim + s_triangle;
+
+                Console.WriteLine($"Figure square is: {s:#.##}");
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine("Invalid input: " + e.Message);
+            }
+        }
+    }
+}
