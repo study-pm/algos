49db10a46c0545694bd599f36e277180ef2dbab2
viktor <sterilework@gmail.com>
2024-06-20 14:16:09 +0300

    feat: add project solution for problem 14

---
+++ src/Exam_Console/14/Program.cs
@@ -0,0 +1,33 @@
+﻿/* Перевести время из минут и секунд в секунды.
+ * Должна осуществляться проверка правильности введенных пользователем данных,
+ * и в случае, если данные неверные, выводить соответствующее сообщение
+ */
+
+using System.ComponentModel;
+
+namespace Console_14
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            try
+            {
+                Console.WriteLine("Enter minutes and seconds separated by a colon symbol (\":\")");
+                string input = Console.ReadLine();
+
+                TimeSpan parsed = TimeSpan.Parse("00:" + input);
+
+                // Doesn't handle the case of mins or seconds exceeding 60
+                // TimeOnly time = new TimeOnly(0, 6, 2);
+                TimeSpan span = new TimeSpan(0, 66, 2);
+
+                Console.WriteLine(Environment.NewLine + "Total seconds: " + parsed.TotalSeconds);
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine("Invalid input: " + e.Message);
+            }
+        }
+    }
+}
