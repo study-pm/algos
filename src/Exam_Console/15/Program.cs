ea4f711356bfaad697579a546ec16f6d4f0c9850
viktor <sterilework@gmail.com>
2024-06-20 18:52:50 +0300

    feat: add project solution for problem 15

---
+++ src/Exam_Console/15/Program.cs
@@ -0,0 +1,32 @@
+﻿/* Перераспределите значения переменных х и у так,
+ * чтобы в х оказалось большее из этих значений, а в у меньшее.
+ */
+
+namespace Console_15
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string msg = "Must be a valid number";
+            try
+            {
+                Console.Write($"Enter x ({msg}): ");
+                double x = double.Parse(Console.ReadLine());
+
+                Console.Write($"Enter y ({msg}): ");
+                double y = double.Parse(Console.ReadLine());
+                if (x < y)
+                {
+                    (x, y) = (y, x);
+                }
+                Console.WriteLine("Result: x = {0}, y = {1}", x, y);
+
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine("Invalid input: " + ex.Message);
+            }
+        }
+    }
+}
