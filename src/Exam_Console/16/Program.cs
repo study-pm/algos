8daf3c961a9ef66244b906e8a90e096900af21b0
viktor <sterilework@gmail.com>
2024-06-20 20:34:05 +0300

    feat: project solution for problem 16

---
+++ src/Exam_Console/16/Program.cs
@@ -0,0 +1,33 @@
+﻿/* Вывести сумму первых n членов ряда: 1 + 1/2 + 1/3 + 1/4 + ...
+ * Количество суммируемых членов ряда задается пользователем.
+ */
+
+namespace Console_16
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            try
+            {
+                Console.Write("Enter natural number: ");
+                int n = int.Parse(Console.ReadLine());
+
+                if (n < 0) throw new FormatException("Must be a natural number");
+
+                double sum = 0;
+
+                for (int i = 1; i <= n; i++)
+                {
+                    sum += 1.0/i;
+                }
+
+                Console.WriteLine("Series sum is: {0:N2}", sum);
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine("Invalid input: " + ex.Message);
+            }
+        }
+    }
+}
