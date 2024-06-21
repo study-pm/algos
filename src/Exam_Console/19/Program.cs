65c802b9b9aec1e5582d5baa7ce5403e28b2733c
viktor <sterilework@gmail.com>
2024-06-21 19:48:09 +0300

    feat: add project solution for problem 19

---
+++ src/Exam_Console/19/Program.cs
@@ -0,0 +1,36 @@
+﻿/* Вывести все нечётные и кратные трём числа из диапазона [a; b] */
+
+namespace Console_19
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string msg = "Must be a valid integer number";
+
+            try
+            {
+                Console.Write($"Enter a ({msg}): ");
+                int a = int.Parse(Console.ReadLine());
+
+                Console.Write($"Enter b ({msg}): ");
+                int b = int.Parse(Console.ReadLine());
+
+                if (a > b) (a, b) = (b, a);
+
+                int val = a;
+
+                while (val <= b)
+                {
+                    if (val % 2 != 0) Console.WriteLine("Odd: " + val);
+                    if (val % 3 == 0) Console.WriteLine("Multiple of 3: " + val);
+                    val++;
+                }
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine("Invalid input: " + ex.Message);
+            }
+        }
+    }
+}
