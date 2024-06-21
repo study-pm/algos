1888ea845d046cdf1e58d8ddbf52dc27183fc4dc
viktor <sterilework@gmail.com>
2024-06-22 00:08:46 +0300

    feat: add project solution for problem 20

---
+++ src/Exam_Console/20/Program.cs
@@ -0,0 +1,36 @@
+﻿/* Вывести все чётные и кратные четырём числа из диапазона [a; b]. */
+
+namespace Console_20
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string msg = "Must be a valid integer number";
+
+            try
+            {
+                Console.Write($"Enter a ({msg}): ");
+                int a = int.Parse(Console.ReadLine());
+
+                Console.Write($"Enter b ({msg}): ");
+                int b = int.Parse(Console.ReadLine());
+
+                if (a > b) (a, b) = (b, a);
+
+                int val = a;
+
+                while (val <= b)
+                {
+                    if (val % 2 == 0) Console.WriteLine("Even: " + val);
+                    if (val % 4 == 0) Console.WriteLine("Multiple of 4: " + val);
+                    val++;
+                }
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine("Invalid input: " + ex.Message);
+            }
+        }
+    }
+}
