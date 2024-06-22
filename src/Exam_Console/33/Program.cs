a1ef2d65f193a2edfad1a68364aaee0fa2e077dc
viktor <sterilework@gmail.com>
2024-06-23 02:35:32 +0300

    feat: add project solution for problem 33

---
+++ src/Exam_Console/33/Program.cs
@@ -0,0 +1,30 @@
+﻿/* Вычислить m = max(x+y^2, y+z^2) + 1. */
+
+namespace Console_33
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            try
+            {
+                Console.Write("Enter number x: ");
+                double x = double.Parse(Console.ReadLine());
+
+                Console.Write("Enter number y: ");
+                double y = double.Parse(Console.ReadLine());
+
+                Console.Write("Enter number z: ");
+                double z = double.Parse(Console.ReadLine());
+
+                double m = Math.Max(x + y * y, y + z * z) + 1;
+
+                Console.WriteLine("m = {0:.##}", m);
+            }
+            catch (Exception exc)
+            {
+                Console.WriteLine("Invalid input: " + exc.Message);
+            }
+        }
+    }
+}
