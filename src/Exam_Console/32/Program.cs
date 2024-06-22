0f3c4d75badc34967143bd624b139dc618e3c5a2
viktor <sterilework@gmail.com>
2024-06-23 02:11:51 +0300

    feat: add project solution for problem 32

---
+++ src/Exam_Console/32/Program.cs
@@ -0,0 +1,36 @@
+﻿/* Вывести максимальное число из вводимой с клавиатуры последовательности чисел.
+ * Признаком конца ввода является число 9999.
+ */
+
+
+namespace Console_32
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int max = int.MinValue;
+            int count = 0;
+
+            Console.WriteLine("Enter integer numbers one by one, to end series enter 9999");
+
+            try
+            {
+                for (; ; )
+                {
+                    int n = int.Parse(Console.ReadLine());
+                    if (n == 9999) break;
+                    if (max < n) max = n;
+                    count++;
+                }
+
+                if (count == 0) Console.WriteLine("Series has no items, hense no maximum value.");
+                else Console.WriteLine("Series maximum value is {0}", max);
+            }
+            catch (Exception exc)
+            {
+                Console.WriteLine("Invalid input: " + exc.Message);
+            }
+        }
+    }
+}
