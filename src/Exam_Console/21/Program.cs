ebdc4da40721f6965234e08515037aba1fa34476
viktor <sterilework@gmail.com>
2024-06-22 11:46:47 +0300

    feat: add project solutions for problems 21 and 22

---
+++ src/Exam_Console/21/Program.cs
@@ -0,0 +1,71 @@
+﻿/* Услуги телефонной сети оплачиваются по следующему правилу:
+ * за разговоры от А минут в месяц — В рублей,
+ * а разговоры сверх установленной нормы оплачиваются из расчета С рублей за минуту.
+ * Вычислить плату за пользование телефоном для введенного времени разговоров за месяц.
+ */
+
+using System.Globalization;
+
+namespace Console_21
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            // Console output setup
+            CultureInfo cultureInfo = CultureInfo.GetCultureInfo("ru-RU");
+            // Correct display russian rouble symbol
+            Console.OutputEncoding = System.Text.Encoding.Unicode;
+
+            try
+            {
+                string msg = "Must be a valid non-negative integer";
+                Console.Write($"Enter monthly phone time limit A in minutes ({msg}): ");
+                int lmtA = int.Parse(Console.ReadLine());
+
+                Console.Write($"Enter rate B in roubles ({msg}): ");
+                int rateB = int.Parse(Console.ReadLine());
+
+                Console.Write($"Enter rate C (for extra time) in roubles ({msg}): ");
+                int rateC = int.Parse(Console.ReadLine());
+
+                Console.Write($"Enter actual monthly phone time in time format (hh:mm): ");
+                TimeSpan time = TimeSpan.Parse(Console.ReadLine());
+
+                int totalTime = (int)time.TotalMinutes;
+                int inTime;
+                int extraTime;
+
+                if (totalTime < lmtA)
+                {
+                    inTime = totalTime;
+                    extraTime = 0;
+                }
+                else
+                {
+                    inTime = lmtA;
+                    extraTime = totalTime - inTime;
+
+                }
+
+                int totalCharge = inTime * rateB + extraTime * rateC;
+
+                string formatted = string.Format(cultureInfo, "{0:C2}", totalCharge);
+
+                Console.WriteLine("Total monthly charge is: {0}", formatted);
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine("Invalid input: " + ex.Message);
+            }
+        }
+    }
+}
+
+/* Sample data
+ * A = 60
+ * B = 100
+ * C = 150
+ * actual = 2:22
+ * totalCharge = 6 000 + 12 300 = 18 300,00 ₽
+ */
