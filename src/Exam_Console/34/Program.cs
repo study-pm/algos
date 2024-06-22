ec52de56c403ccf64eaf3ac1862368baf5f2ab87
viktor <sterilework@gmail.com>
2024-06-23 02:57:25 +0300

    feat: add project solution for problem 34

---
+++ src/Exam_Console/34/Program.cs
@@ -0,0 +1,30 @@
+﻿/* Вычислить m = max(x^2+y^2, y^2+z^2) - 4. */
+
+namespace Console_34
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            try
+            {
+                Console.Write("Enter number x: ");
+                double x = double.Parse(Console.ReadLine());
+
+                Console.Write("Enter number y: ");
+                double y = double.Parse(Console.ReadLine());
+
+                Console.Write("Enter number z: ");
+                double z = double.Parse(Console.ReadLine());
+
+                double m = Math.Max(x*x + y*y, y*y + z*z) - 4;
+
+                Console.WriteLine("m = {0:.##}", m);
+            }
+            catch (Exception exc)
+            {
+                Console.WriteLine("Invalid input: " + exc.Message);
+            }
+        }
+    }
+}
