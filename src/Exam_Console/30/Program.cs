b3637dfdeb6b429e6d6ac1b75b64a7338d6b20bd
viktor <sterilework@gmail.com>
2024-06-23 00:12:01 +0300

    feat: add project solution for problem 30

---
+++ src/Exam_Console/30/Program.cs
@@ -0,0 +1,30 @@
+﻿/* Вычислить m = min(x^2+y^2, y^2+z^2) - 4 */
+
+namespace Console_30
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            try
+            {
+                Console.Write("Enter number x: ");
+                double x = double.Parse(Console.ReadLine());
+
+                Console.Write("Enter number y: ");
+                double y = double.Parse(Console.ReadLine());
+
+                Console.Write("Enter number z: ");
+                double z = double.Parse(Console.ReadLine());
+
+                double m = Math.Min(x * x + y * y, y * y + z * z) - 4;
+
+                Console.WriteLine($"m = {m:G2}");
+            }
+            catch (Exception exc)
+            {
+                Console.WriteLine("Invalid input: " + exc.Message);
+            }
+        }
+    }
+}
