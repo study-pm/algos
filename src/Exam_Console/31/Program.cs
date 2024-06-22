356c62a11f06317f9c2c8a30fc3d6f6984db980a
viktor <sterilework@gmail.com>
2024-06-23 01:33:26 +0300

    feat: add project solution for problem 31

---
+++ src/Exam_Console/31/Program.cs
@@ -0,0 +1,35 @@
+﻿/* Вывести 	минимальное число из вводимой с клавиатуры последовательности чисел.
+ * Признаком конца ввода является число 9999.
+ */
+
+
+namespace Console_31
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            try
+            {
+                int min = int.MaxValue;
+                int count = 0;
+
+                Console.WriteLine("Enter integer numbers one by one, to end series enter 9999");
+                while (true)
+                {
+                    int n = int.Parse(Console.ReadLine());
+                    if (n == 9999) break;
+                    count++;
+                    if (n < min) min = n;
+                }
+
+                if (count == 0) Console.WriteLine("The series has 0 items, hense no minimum value");
+                else Console.WriteLine("The mininum of the series is {0}", min);
+            }
+            catch (Exception exc)
+            {
+                Console.WriteLine("Invalid input: " + exc.Message);
+            }
+        }
+    }
+}
