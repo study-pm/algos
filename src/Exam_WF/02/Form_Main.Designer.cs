e2a908f8c71a7cd16e50d6eefa1013e5f0180e0a
viktor <sterilework@gmail.com>
2024-06-23 16:55:49 +0300

    feat: add project solution for problem 2

---
+++ src/Exam_WF/02/Form_Main.Designer.cs
@@ -0,0 +1,163 @@
+﻿namespace WF_02
+{
+    partial class Form_Main
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label_m = new System.Windows.Forms.Label();
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.textBox_m = new System.Windows.Forms.TextBox();
+            this.textBox_n = new System.Windows.Forms.TextBox();
+            this.label_n = new System.Windows.Forms.Label();
+            this.button_Calculate = new System.Windows.Forms.Button();
+            this.groupBox2 = new System.Windows.Forms.GroupBox();
+            this.textBox_y = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.groupBox1.SuspendLayout();
+            this.groupBox2.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // label_m
+            // 
+            this.label_m.AutoSize = true;
+            this.label_m.Location = new System.Drawing.Point(6, 30);
+            this.label_m.Name = "label_m";
+            this.label_m.Size = new System.Drawing.Size(50, 13);
+            this.label_m.TabIndex = 0;
+            this.label_m.Text = "Число m";
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.Controls.Add(this.textBox_n);
+            this.groupBox1.Controls.Add(this.label_n);
+            this.groupBox1.Controls.Add(this.textBox_m);
+            this.groupBox1.Controls.Add(this.label_m);
+            this.groupBox1.Location = new System.Drawing.Point(13, 13);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Size = new System.Drawing.Size(173, 88);
+            this.groupBox1.TabIndex = 1;
+            this.groupBox1.TabStop = false;
+            this.groupBox1.Text = "Исходные данные";
+            // 
+            // textBox_m
+            // 
+            this.textBox_m.Location = new System.Drawing.Point(62, 27);
+            this.textBox_m.Name = "textBox_m";
+            this.textBox_m.Size = new System.Drawing.Size(100, 20);
+            this.textBox_m.TabIndex = 1;
+            this.textBox_m.Text = "0";
+            this.textBox_m.TextChanged += new System.EventHandler(this.textBox_m_TextChanged);
+            // 
+            // textBox_n
+            // 
+            this.textBox_n.Location = new System.Drawing.Point(62, 53);
+            this.textBox_n.Name = "textBox_n";
+            this.textBox_n.Size = new System.Drawing.Size(100, 20);
+            this.textBox_n.TabIndex = 3;
+            this.textBox_n.Text = "0";
+            this.textBox_n.TextChanged += new System.EventHandler(this.textBox_n_TextChanged);
+            // 
+            // label_n
+            // 
+            this.label_n.AutoSize = true;
+            this.label_n.Location = new System.Drawing.Point(6, 56);
+            this.label_n.Name = "label_n";
+            this.label_n.Size = new System.Drawing.Size(48, 13);
+            this.label_n.TabIndex = 2;
+            this.label_n.Text = "Число n";
+            // 
+            // button_Calculate
+            // 
+            this.button_Calculate.Location = new System.Drawing.Point(13, 108);
+            this.button_Calculate.Name = "button_Calculate";
+            this.button_Calculate.Size = new System.Drawing.Size(173, 23);
+            this.button_Calculate.TabIndex = 2;
+            this.button_Calculate.Text = "Вычислить y";
+            this.button_Calculate.UseVisualStyleBackColor = true;
+            this.button_Calculate.Click += new System.EventHandler(this.button_Calculate_Click);
+            // 
+            // groupBox2
+            // 
+            this.groupBox2.Controls.Add(this.textBox_y);
+            this.groupBox2.Controls.Add(this.label1);
+            this.groupBox2.Location = new System.Drawing.Point(192, 13);
+            this.groupBox2.Name = "groupBox2";
+            this.groupBox2.Size = new System.Drawing.Size(200, 88);
+            this.groupBox2.TabIndex = 3;
+            this.groupBox2.TabStop = false;
+            this.groupBox2.Text = "Результат";
+            // 
+            // textBox_y
+            // 
+            this.textBox_y.Location = new System.Drawing.Point(59, 27);
+            this.textBox_y.Name = "textBox_y";
+            this.textBox_y.ReadOnly = true;
+            this.textBox_y.Size = new System.Drawing.Size(100, 20);
+            this.textBox_y.TabIndex = 5;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(3, 30);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(47, 13);
+            this.label1.TabIndex = 4;
+            this.label1.Text = "Число y";
+            // 
+            // Form_Main
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.groupBox2);
+            this.Controls.Add(this.button_Calculate);
+            this.Controls.Add(this.groupBox1);
+            this.Name = "Form_Main";
+            this.Text = "02";
+            this.Load += new System.EventHandler(this.Form_Main_Load);
+            this.groupBox1.ResumeLayout(false);
+            this.groupBox1.PerformLayout();
+            this.groupBox2.ResumeLayout(false);
+            this.groupBox2.PerformLayout();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label_m;
+        private System.Windows.Forms.GroupBox groupBox1;
+        private System.Windows.Forms.TextBox textBox_n;
+        private System.Windows.Forms.Label label_n;
+        private System.Windows.Forms.TextBox textBox_m;
+        private System.Windows.Forms.Button button_Calculate;
+        private System.Windows.Forms.GroupBox groupBox2;
+        private System.Windows.Forms.TextBox textBox_y;
+        private System.Windows.Forms.Label label1;
+    }
+}
+
---
+++ src/Exam_WF/02/Form_Main.cs
@@ -0,0 +1,72 @@
+﻿/* Вычислить значение функции y = (3n!-4m!)/(3 * (5 - m)!),
+ * где n, m вводятся с клавиатуры.
+ * Для вычисления факториала числа использовать рекурсивную функцию
+ * k! = 1, if k = 0,
+ * k! = k * (k-1)!, if k >= 1
+ */
+
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Globalization;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace WF_02
+{
+    public partial class Form_Main : Form
+    {
+        CultureInfo ci = CultureInfo.GetCultureInfo("ru-RU");
+
+        uint m = 0;
+        uint n = 0;
+        // Факториа́л — функция, определённая на множестве неотрицательных целых чисел.
+        static double Factorial(uint k)
+        {
+            if (k == 0) return 1;
+            return k * Factorial(--k);
+        }
+        public Form_Main()
+        {
+            InitializeComponent();
+
+            textBox_m.Validating += ValidateInt;
+        }
+        private void ValidateInt(object sender, CancelEventArgs e)
+        {
+            var tb = sender as TextBox;
+            int dummy;
+            e.Cancel = !int.TryParse(tb.Text, out dummy);
+            tb.Select(0, tb.Text.Length);
+        }
+        private void Form_Main_Load(object sender, EventArgs e)
+        {
+        }
+
+        private void button_Calculate_Click(object sender, EventArgs e)
+        {
+            if (m > 5)
+            {
+                MessageBox.Show("Значение m должно быть в диапазоне от 0 до 5");
+                return;
+            }
+
+            double y = (3 * Factorial(n) - 4 * Factorial(m)) / (3 * Factorial(5 - m));
+            textBox_y.Text = string.Format(ci, "{0:N2}", y);
+        }
+
+        private void textBox_m_TextChanged(object sender, EventArgs e)
+        {
+            uint.TryParse(textBox_m.Text, out m);
+        }
+
+        private void textBox_n_TextChanged(object sender, EventArgs e)
+        {
+            uint.TryParse(textBox_n.Text, out n);
+        }
+    }
+}
