acf27543b876b1aa9bf9ef49320767d45db6c898
viktor <sterilework@gmail.com>
2024-06-24 22:57:11 +0300

    feat: add structural parse, file i/o

--- src/Exam_Console/04/Program.cs
+++ src/Exam_Console/04/Program.cs
@@ -4,30 +4,70 @@
  * цена 1 литра бензина.
  */
 
+using System.Globalization;
+using System.Runtime.CompilerServices;
+
 namespace Console_04
 {
+    internal struct NonNegative
+    {
+        public double Value { get; set; }
+        public NonNegative(double val)
+        {
+            Value = val;
+        }
+        public override string ToString() => Value.ToString();
+        public static implicit operator double(NonNegative val) => val.Value;
+        public static implicit operator NonNegative(double val) => new NonNegative(val);
+        public static NonNegative Parse(string s)
+        {
+            double res = double.Parse(s);
+            if (res < 0) throw new FormatException($"The input string {s} was not in a correct format. Must be a non-negative value.");
+            return new NonNegative(res);
+        }
+    }
     internal class Program
     {
         static void Main(string[] args)
         {
+            // Setup console output
+            Console.OutputEncoding = System.Text.Encoding.Unicode;
+            CultureInfo ci = CultureInfo.GetCultureInfo("ru-RU");
+
             try
             {
                 Console.Write("Enter distance in km: ");
-                double distance = double.Parse(Console.ReadLine());
+                NonNegative distance = NonNegative.Parse(Console.ReadLine());
                 Console.Write("Enter gas consumption in l/100km: ");
-                double consumption = double.Parse(Console.ReadLine());
+                NonNegative consumption = NonNegative.Parse(Console.ReadLine());
                 Console.Write("Enter gas price for 1 liter in roubles: ");
-                double price = double.Parse(Console.ReadLine());
-
-                if (distance < 0 || consumption < 0 || price < 0) throw new Exception("Must be a non-negative number");
+                NonNegative price = NonNegative.Parse(Console.ReadLine());
 
                 double cost = distance * consumption / 100 * price * 2;
 
                 Console.WriteLine("Trip cost (hin und zurück): {0:.##}", cost);
+
+                string path = "result.txt";
+                File.WriteAllText(path, cost.ToString());
+                string resOut = File.ReadAllText(path);
             }
-            catch (Exception e)
+            catch (Exception exc)
             {
-                Console.WriteLine("Invalid input: " + e.Message);
+                Console.WriteLine("Invalid input: " + exc.Message);
+
+                string path = "error.txt";
+                string[] errTxt =
+                {
+                    "Time: " + DateTime.Now.ToString(ci),
+                    "Error: Invalid input",
+                    "Type: " + exc.GetType().Name,
+                    "Message: " + exc.Message,
+                    "Stack trace: " + exc.StackTrace,
+                };
+
+                File.WriteAllLines(path, errTxt);
+
+                string[] errOut = File.ReadAllLines(path);
             }
         }
     }
