13b8cb92cad1cd5c8463280eebda8cf5675d1237
viktor <sterilework@gmail.com>
2024-06-26 12:49:00 +0300

    feat: add project solution for wf problem 18

---
+++ src/Exam_WF/18/Form_Main.Designer.cs
@@ -0,0 +1,98 @@
+﻿namespace WF_18
+{
+    partial class Form_Main
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.dataGridView_in = new System.Windows.Forms.DataGridView();
+            this.button_submit = new System.Windows.Forms.Button();
+            this.dataGridView_out = new System.Windows.Forms.DataGridView();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView_in)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView_out)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // dataGridView_in
+            // 
+            this.dataGridView_in.AutoSizeColumnsMode = System.Windows.Forms.DataGridViewAutoSizeColumnsMode.AllCells;
+            this.dataGridView_in.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView_in.Location = new System.Drawing.Point(12, 12);
+            this.dataGridView_in.Name = "dataGridView_in";
+            this.dataGridView_in.ScrollBars = System.Windows.Forms.ScrollBars.Horizontal;
+            this.dataGridView_in.Size = new System.Drawing.Size(240, 130);
+            this.dataGridView_in.TabIndex = 6;
+            this.dataGridView_in.CellEndEdit += new System.Windows.Forms.DataGridViewCellEventHandler(this.dataGridView_in_CellEndEdit);
+            this.dataGridView_in.CellValidating += new System.Windows.Forms.DataGridViewCellValidatingEventHandler(this.dataGridView_in_CellValidating);
+            this.dataGridView_in.CellValueChanged += new System.Windows.Forms.DataGridViewCellEventHandler(this.dataGridView_in_CellValueChanged);
+            this.dataGridView_in.CurrentCellDirtyStateChanged += new System.EventHandler(this.dataGridView_in_CurrentCellDirtyStateChanged);
+            // 
+            // button_submit
+            // 
+            this.button_submit.Location = new System.Drawing.Point(12, 148);
+            this.button_submit.Name = "button_submit";
+            this.button_submit.Size = new System.Drawing.Size(239, 23);
+            this.button_submit.TabIndex = 9;
+            this.button_submit.Text = "Submit";
+            this.button_submit.UseVisualStyleBackColor = true;
+            this.button_submit.Click += new System.EventHandler(this.button_submit_Click);
+            // 
+            // dataGridView_out
+            // 
+            this.dataGridView_out.AllowUserToAddRows = false;
+            this.dataGridView_out.AllowUserToDeleteRows = false;
+            this.dataGridView_out.AutoSizeColumnsMode = System.Windows.Forms.DataGridViewAutoSizeColumnsMode.AllCells;
+            this.dataGridView_out.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView_out.Location = new System.Drawing.Point(259, 13);
+            this.dataGridView_out.Name = "dataGridView_out";
+            this.dataGridView_out.ReadOnly = true;
+            this.dataGridView_out.ScrollBars = System.Windows.Forms.ScrollBars.None;
+            this.dataGridView_out.Size = new System.Drawing.Size(240, 129);
+            this.dataGridView_out.TabIndex = 10;
+            // 
+            // Form_Main
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.dataGridView_out);
+            this.Controls.Add(this.button_submit);
+            this.Controls.Add(this.dataGridView_in);
+            this.Name = "Form_Main";
+            this.Text = "18";
+            this.Load += new System.EventHandler(this.Form_Main_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView_in)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView_out)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+        private System.Windows.Forms.DataGridView dataGridView_in;
+        private System.Windows.Forms.Button button_submit;
+        private System.Windows.Forms.DataGridView dataGridView_out;
+    }
+}
+
---
+++ src/Exam_WF/18/Form_Main.cs
@@ -0,0 +1,112 @@
+﻿/* Ввести по строкам с клавиатуры двумерный массив (4×3) и вычислить сумму его элементов по строкам. */
+
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace WF_18
+{
+    public partial class Form_Main : Form
+    {
+        public Form_Main()
+        {
+            InitializeComponent();
+        }
+
+        private void Submit()
+        {
+            try
+            {
+                for (int i = 0; i < dataGridView_in.Rows.Count; i++)
+                {
+                    double sum = 0;
+                    for (int j = 0; j < dataGridView_in.Columns.Count; j++)
+                    {
+                        double val = double.Parse(dataGridView_in[j, i].Value.ToString());
+                        sum += val;
+                    }
+                    dataGridView_out[0, i].Value = sum.ToString();
+                }
+                ToggleOut(true);
+            }
+            catch (NullReferenceException)
+            {
+                MessageBox.Show("Invalid input: all cells must be provided with values");
+            }
+            catch (Exception exc)
+            {
+                MessageBox.Show("Invalid input: " + exc.Message);
+            }
+        }
+        private void ToggleOut(bool isOn)
+        {
+            dataGridView_out.Visible = isOn;
+        }
+        private void Form_Main_Load(object sender, EventArgs e)
+        {
+            ToggleOut(false);
+
+            dataGridView_in.ColumnCount = 4;
+            dataGridView_in.RowCount = 4;
+            dataGridView_in.AllowUserToAddRows = false;
+            for (int i = 0; i < dataGridView_in.RowCount; i++)
+            {
+                dataGridView_in.Rows[i].HeaderCell.Value = i.ToString();
+            }
+            for (int i = 0; i < dataGridView_in.ColumnCount; i++)
+            {
+                dataGridView_in.Columns[i].HeaderCell.Value = i.ToString();
+                dataGridView_in.Columns[i].SortMode = DataGridViewColumnSortMode.NotSortable;
+            }
+
+            dataGridView_out.ColumnCount = 1;
+            dataGridView_out.RowCount = 3;
+            dataGridView_out.AllowUserToAddRows = false;
+            dataGridView_out.Columns[0].HeaderCell.Value = "Item sum";
+            dataGridView_out.Columns[0].SortMode = DataGridViewColumnSortMode.NotSortable;
+            for (int i = 0; i < dataGridView_out.RowCount; i++)
+            {
+                dataGridView_out.Rows[i].HeaderCell.Value = i.ToString();
+            }
+        }
+
+        private void dataGridView_in_CellEndEdit(object sender, DataGridViewCellEventArgs e)
+        {
+            Control ctrl = sender as Control;
+            try
+            {
+                double.Parse(dataGridView_in.CurrentCell.Value.ToString());
+                dataGridView_in.CurrentCell.Style.BackColor = Color.LightCyan;
+            }
+            catch
+            {
+                dataGridView_in.CurrentCell.Style.BackColor = Color.LightPink;
+            }
+
+            // MessageBox.Show(dataGridView_in.CurrentCell.ColumnIndex + " " + dataGridView_in.CurrentCell.Value.ToString());
+        }
+
+        private void dataGridView_in_CurrentCellDirtyStateChanged(object sender, EventArgs e)
+        {
+        }
+
+        private void dataGridView_in_CellValueChanged(object sender, DataGridViewCellEventArgs e)
+        {
+        }
+
+        private void dataGridView_in_CellValidating(object sender, DataGridViewCellValidatingEventArgs e)
+        {
+        }
+
+        private void button_submit_Click(object sender, EventArgs e)
+        {
+            Submit();
+        }
+    }
+}
