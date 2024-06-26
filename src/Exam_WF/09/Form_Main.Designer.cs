9f9ec4c87debeec9fbdffaa8fee5ae3c44a5e455
viktor <sterilework@gmail.com>
2024-06-26 10:36:44 +0300

    feat: add project solution for wf problem 9

---
+++ src/Exam_WF/09/Form_Main.Designer.cs
@@ -0,0 +1,128 @@
+﻿namespace WF_09
+{
+    partial class Form_Main
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.groupBox_in = new System.Windows.Forms.GroupBox();
+            this.button_submit = new System.Windows.Forms.Button();
+            this.textBox_m = new System.Windows.Forms.TextBox();
+            this.label_m = new System.Windows.Forms.Label();
+            this.errorProvider_m = new System.Windows.Forms.ErrorProvider(this.components);
+            this.dataGridView_out = new System.Windows.Forms.DataGridView();
+            this.groupBox_in.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.errorProvider_m)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView_out)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // groupBox_in
+            // 
+            this.groupBox_in.Controls.Add(this.button_submit);
+            this.groupBox_in.Controls.Add(this.textBox_m);
+            this.groupBox_in.Controls.Add(this.label_m);
+            this.groupBox_in.Location = new System.Drawing.Point(13, 13);
+            this.groupBox_in.Name = "groupBox_in";
+            this.groupBox_in.Size = new System.Drawing.Size(200, 100);
+            this.groupBox_in.TabIndex = 0;
+            this.groupBox_in.TabStop = false;
+            this.groupBox_in.Text = "Input";
+            // 
+            // button_submit
+            // 
+            this.button_submit.Location = new System.Drawing.Point(7, 71);
+            this.button_submit.Name = "button_submit";
+            this.button_submit.Size = new System.Drawing.Size(187, 23);
+            this.button_submit.TabIndex = 2;
+            this.button_submit.Text = "Submit";
+            this.button_submit.UseVisualStyleBackColor = true;
+            this.button_submit.Click += new System.EventHandler(this.button_submit_Click);
+            // 
+            // textBox_m
+            // 
+            this.textBox_m.Location = new System.Drawing.Point(133, 17);
+            this.textBox_m.Name = "textBox_m";
+            this.textBox_m.Size = new System.Drawing.Size(44, 20);
+            this.textBox_m.TabIndex = 1;
+            this.textBox_m.TextChanged += new System.EventHandler(this.textBox_m_TextChanged);
+            this.textBox_m.KeyPress += new System.Windows.Forms.KeyPressEventHandler(this.textBox_m_KeyPress);
+            this.textBox_m.Validating += new System.ComponentModel.CancelEventHandler(this.textBox_m_Validating);
+            // 
+            // label_m
+            // 
+            this.label_m.AutoSize = true;
+            this.label_m.Location = new System.Drawing.Point(7, 20);
+            this.label_m.Name = "label_m";
+            this.label_m.Size = new System.Drawing.Size(120, 13);
+            this.label_m.TabIndex = 0;
+            this.label_m.Text = "Enter an integer number";
+            // 
+            // errorProvider_m
+            // 
+            this.errorProvider_m.ContainerControl = this;
+            // 
+            // dataGridView_out
+            // 
+            this.dataGridView_out.AllowUserToAddRows = false;
+            this.dataGridView_out.AllowUserToDeleteRows = false;
+            this.dataGridView_out.AutoSizeColumnsMode = System.Windows.Forms.DataGridViewAutoSizeColumnsMode.AllCells;
+            this.dataGridView_out.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView_out.Location = new System.Drawing.Point(12, 119);
+            this.dataGridView_out.Name = "dataGridView_out";
+            this.dataGridView_out.ReadOnly = true;
+            this.dataGridView_out.Size = new System.Drawing.Size(776, 150);
+            this.dataGridView_out.TabIndex = 0;
+            // 
+            // Form_Main
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.dataGridView_out);
+            this.Controls.Add(this.groupBox_in);
+            this.Name = "Form_Main";
+            this.Text = "09";
+            this.Load += new System.EventHandler(this.Form_Main_Load);
+            this.groupBox_in.ResumeLayout(false);
+            this.groupBox_in.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.errorProvider_m)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView_out)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.GroupBox groupBox_in;
+        private System.Windows.Forms.TextBox textBox_m;
+        private System.Windows.Forms.Label label_m;
+        private System.Windows.Forms.ErrorProvider errorProvider_m;
+        private System.Windows.Forms.Button button_submit;
+        private System.Windows.Forms.DataGridView dataGridView_out;
+    }
+}
+
---
+++ src/Exam_WF/09/Form_Main.cs
@@ -0,0 +1,105 @@
+﻿/* Вывести таблицу степеней числа m^n, где n изменяется от -10 до 10,
+ * m - целое число, вводится с клавиатуры.
+ * Для вычисления степени числа использовать рекурсивную функцию.
+ */
+
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Globalization;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace WF_09
+{
+    public partial class Form_Main : Form
+    {
+        private readonly int[] range = { -10, 10 };
+        private int count = 0;
+        public Form_Main()
+        {
+            InitializeComponent();
+        }
+
+        private double GetPower(double a, int k)
+        {
+            if (k == 0) return 1;
+            if (k < 0) return GetPower(1/a, -k);
+            return a * GetPower(a, --k);
+        }
+        private void Submit()
+        {
+            try
+            {
+                int m = int.Parse(textBox_m.Text);
+                double[] power = new double[count];
+                for (int i = 0, n = range[0]; n <= range[1]; i++, n++)
+                {
+                    power[i] = GetPower(m, n);
+                    dataGridView_out[i, 0].Value = n;
+                    dataGridView_out[i, 1].Value = power[i].ToString(power[i] == 0 ? "N0" : "#.#######", CultureInfo.GetCultureInfo("ru-RU"));
+                }
+                ToggleOut(true);
+            }
+            catch(Exception exc)
+            {
+                MessageBox.Show(exc.Message);
+            }
+        }
+        private void ToggleOut(bool isOn)
+        {
+            dataGridView_out.Visible = isOn;
+        }
+        private void Form_Main_Load(object sender, EventArgs e)
+        {
+            ToggleOut(false);
+            for (int i = range[0]; i <= range[1]; i++) count++;
+            dataGridView_out.RowCount = 2;
+            dataGridView_out.ColumnCount = count;
+        }
+
+
+        private void textBox_m_KeyPress(object sender, KeyPressEventArgs e)
+        {
+            if (e.KeyChar == (char)Keys.Enter) Submit();
+        }
+        private void textBox_m_TextChanged(object sender, EventArgs e)
+        {
+            ToggleOut(false);
+            Control ctrl = sender as Control;
+            errorProvider_m.Clear();
+            try
+            {
+                int.Parse(ctrl.Text);
+                ctrl.BackColor = Color.LightGreen;
+            }
+            catch
+            {
+                ctrl.BackColor = Color.LightPink;
+            }
+        }
+        private void textBox_m_Validating(object sender, CancelEventArgs e)
+        {
+            Control ctrl = sender as Control;
+            ctrl.BackColor = Color.White;
+            try
+            {
+                int.Parse(ctrl.Text);
+                errorProvider_m.Clear();
+            }
+            catch (Exception exc)
+            {
+                errorProvider_m.SetError(ctrl, exc.Message);
+            }
+        }
+
+        private void button_submit_Click(object sender, EventArgs e)
+        {
+            Submit();
+        }
+    }
+}
