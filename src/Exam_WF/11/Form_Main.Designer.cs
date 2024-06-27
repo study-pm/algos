38d912dd7203385e7c5ac619c7cdef3364a8c060
viktor <sterilework@gmail.com>
2024-06-28 00:51:51 +0300

    feat: add project solution for wf problem 11

---
+++ src/Exam_WF/11/Form_Main.Designer.cs
@@ -0,0 +1,231 @@
+﻿namespace WF_11
+{
+    partial class Form_Main
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.groupBox_out = new System.Windows.Forms.GroupBox();
+            this.textBox_B = new System.Windows.Forms.TextBox();
+            this.label_B = new System.Windows.Forms.Label();
+            this.textBox_A = new System.Windows.Forms.TextBox();
+            this.label_A = new System.Windows.Forms.Label();
+            this.groupBox_in = new System.Windows.Forms.GroupBox();
+            this.button_submit = new System.Windows.Forms.Button();
+            this.textBox_n = new System.Windows.Forms.TextBox();
+            this.label_n = new System.Windows.Forms.Label();
+            this.errorProvider_n = new System.Windows.Forms.ErrorProvider(this.components);
+            this.button_save = new System.Windows.Forms.Button();
+            this.dataGridView_A = new System.Windows.Forms.DataGridView();
+            this.dataGridView_B = new System.Windows.Forms.DataGridView();
+            this.groupBox_output = new System.Windows.Forms.GroupBox();
+            this.groupBox_out.SuspendLayout();
+            this.groupBox_in.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.errorProvider_n)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView_A)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView_B)).BeginInit();
+            this.groupBox_output.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // groupBox_out
+            // 
+            this.groupBox_out.Controls.Add(this.button_save);
+            this.groupBox_out.Controls.Add(this.textBox_B);
+            this.groupBox_out.Controls.Add(this.label_B);
+            this.groupBox_out.Controls.Add(this.textBox_A);
+            this.groupBox_out.Controls.Add(this.label_A);
+            this.groupBox_out.Location = new System.Drawing.Point(219, 12);
+            this.groupBox_out.Name = "groupBox_out";
+            this.groupBox_out.Size = new System.Drawing.Size(200, 100);
+            this.groupBox_out.TabIndex = 3;
+            this.groupBox_out.TabStop = false;
+            this.groupBox_out.Text = "Output";
+            // 
+            // textBox_B
+            // 
+            this.textBox_B.Location = new System.Drawing.Point(29, 41);
+            this.textBox_B.Name = "textBox_B";
+            this.textBox_B.ReadOnly = true;
+            this.textBox_B.Size = new System.Drawing.Size(165, 20);
+            this.textBox_B.TabIndex = 5;
+            // 
+            // label_B
+            // 
+            this.label_B.AutoSize = true;
+            this.label_B.Location = new System.Drawing.Point(6, 44);
+            this.label_B.Name = "label_B";
+            this.label_B.Size = new System.Drawing.Size(17, 13);
+            this.label_B.TabIndex = 4;
+            this.label_B.Text = "B:";
+            // 
+            // textBox_A
+            // 
+            this.textBox_A.Location = new System.Drawing.Point(29, 17);
+            this.textBox_A.Name = "textBox_A";
+            this.textBox_A.ReadOnly = true;
+            this.textBox_A.Size = new System.Drawing.Size(165, 20);
+            this.textBox_A.TabIndex = 3;
+            // 
+            // label_A
+            // 
+            this.label_A.AutoSize = true;
+            this.label_A.Location = new System.Drawing.Point(6, 20);
+            this.label_A.Name = "label_A";
+            this.label_A.Size = new System.Drawing.Size(17, 13);
+            this.label_A.TabIndex = 2;
+            this.label_A.Text = "A:";
+            // 
+            // groupBox_in
+            // 
+            this.groupBox_in.Controls.Add(this.button_submit);
+            this.groupBox_in.Controls.Add(this.textBox_n);
+            this.groupBox_in.Controls.Add(this.label_n);
+            this.groupBox_in.Location = new System.Drawing.Point(12, 12);
+            this.groupBox_in.Name = "groupBox_in";
+            this.groupBox_in.Size = new System.Drawing.Size(200, 100);
+            this.groupBox_in.TabIndex = 2;
+            this.groupBox_in.TabStop = false;
+            this.groupBox_in.Text = "Input";
+            // 
+            // button_submit
+            // 
+            this.button_submit.Location = new System.Drawing.Point(6, 68);
+            this.button_submit.Name = "button_submit";
+            this.button_submit.Size = new System.Drawing.Size(188, 23);
+            this.button_submit.TabIndex = 2;
+            this.button_submit.Text = "Submit";
+            this.button_submit.UseVisualStyleBackColor = true;
+            this.button_submit.Click += new System.EventHandler(this.button_submit_Click);
+            // 
+            // textBox_n
+            // 
+            this.textBox_n.Location = new System.Drawing.Point(103, 17);
+            this.textBox_n.Name = "textBox_n";
+            this.textBox_n.Size = new System.Drawing.Size(69, 20);
+            this.textBox_n.TabIndex = 1;
+            this.textBox_n.TextChanged += new System.EventHandler(this.textBox_n_TextChanged);
+            this.textBox_n.KeyPress += new System.Windows.Forms.KeyPressEventHandler(this.textBox_n_KeyPress);
+            this.textBox_n.Validating += new System.ComponentModel.CancelEventHandler(this.textBox_n_Validating);
+            // 
+            // label_n
+            // 
+            this.label_n.AutoSize = true;
+            this.label_n.Location = new System.Drawing.Point(7, 20);
+            this.label_n.Name = "label_n";
+            this.label_n.Size = new System.Drawing.Size(90, 13);
+            this.label_n.TabIndex = 0;
+            this.label_n.Text = "Enter array length";
+            // 
+            // errorProvider_n
+            // 
+            this.errorProvider_n.ContainerControl = this;
+            // 
+            // button_save
+            // 
+            this.button_save.Location = new System.Drawing.Point(6, 68);
+            this.button_save.Name = "button_save";
+            this.button_save.Size = new System.Drawing.Size(188, 23);
+            this.button_save.TabIndex = 6;
+            this.button_save.Text = "Save";
+            this.button_save.UseVisualStyleBackColor = true;
+            this.button_save.Click += new System.EventHandler(this.button_save_Click);
+            // 
+            // dataGridView_A
+            // 
+            this.dataGridView_A.AllowUserToAddRows = false;
+            this.dataGridView_A.AllowUserToDeleteRows = false;
+            this.dataGridView_A.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView_A.Location = new System.Drawing.Point(10, 19);
+            this.dataGridView_A.Name = "dataGridView_A";
+            this.dataGridView_A.ReadOnly = true;
+            this.dataGridView_A.Size = new System.Drawing.Size(300, 150);
+            this.dataGridView_A.TabIndex = 4;
+            // 
+            // dataGridView_B
+            // 
+            this.dataGridView_B.AllowUserToAddRows = false;
+            this.dataGridView_B.AllowUserToDeleteRows = false;
+            this.dataGridView_B.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView_B.Location = new System.Drawing.Point(316, 19);
+            this.dataGridView_B.Name = "dataGridView_B";
+            this.dataGridView_B.ReadOnly = true;
+            this.dataGridView_B.Size = new System.Drawing.Size(300, 150);
+            this.dataGridView_B.TabIndex = 5;
+            // 
+            // groupBox_output
+            // 
+            this.groupBox_output.Controls.Add(this.dataGridView_A);
+            this.groupBox_output.Controls.Add(this.dataGridView_B);
+            this.groupBox_output.Location = new System.Drawing.Point(12, 119);
+            this.groupBox_output.Name = "groupBox_output";
+            this.groupBox_output.Size = new System.Drawing.Size(776, 180);
+            this.groupBox_output.TabIndex = 6;
+            this.groupBox_output.TabStop = false;
+            this.groupBox_output.Text = "Output";
+            // 
+            // Form_Main
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.groupBox_output);
+            this.Controls.Add(this.groupBox_out);
+            this.Controls.Add(this.groupBox_in);
+            this.Name = "Form_Main";
+            this.Text = "11";
+            this.Load += new System.EventHandler(this.Form_Main_Load);
+            this.groupBox_out.ResumeLayout(false);
+            this.groupBox_out.PerformLayout();
+            this.groupBox_in.ResumeLayout(false);
+            this.groupBox_in.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.errorProvider_n)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView_A)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView_B)).EndInit();
+            this.groupBox_output.ResumeLayout(false);
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.GroupBox groupBox_out;
+        private System.Windows.Forms.TextBox textBox_B;
+        private System.Windows.Forms.Label label_B;
+        private System.Windows.Forms.TextBox textBox_A;
+        private System.Windows.Forms.Label label_A;
+        private System.Windows.Forms.GroupBox groupBox_in;
+        private System.Windows.Forms.Button button_submit;
+        private System.Windows.Forms.TextBox textBox_n;
+        private System.Windows.Forms.Label label_n;
+        private System.Windows.Forms.ErrorProvider errorProvider_n;
+        private System.Windows.Forms.Button button_save;
+        private System.Windows.Forms.DataGridView dataGridView_A;
+        private System.Windows.Forms.DataGridView dataGridView_B;
+        private System.Windows.Forms.GroupBox groupBox_output;
+    }
+}
+
---
+++ src/Exam_WF/11/Form_Main.cs
@@ -0,0 +1,207 @@
+﻿/* В массиве целых чисел размерности n (n вводится пользователем) заменить все чётные числа,
+ * значения которых меньше среднего арифметического элементов массива на максимальный элемент массива.
+ */
+
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Globalization;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace WF_11
+{
+    public partial class Form_Main : Form
+    {
+        int[] range = new int[2];
+        Random random = new Random();
+        readonly int maxN = 500;
+        int[] A;
+        int[] B;
+        private int[] GenerateArray(int n, int[] range)
+        {
+            int[] res = new int[n];
+            for (int i = 0; i < n; i++)
+            {
+                res[i] = random.Next(range[0], range[1]);
+            }
+            return res;
+        }
+        private int GetArrayMax(int[] arr)
+        {
+            int max = arr[0];
+            for (int i = 0; i < arr.Length; i++)
+            {
+                if (arr[i] > max) max = arr[i];
+            }
+            return max;
+        }
+        private double GetArrayMean(int[] arr)
+        {
+            int sum = 0;
+            for (int i = 0; i < arr.Length; i++)
+            {
+                sum += arr[i];
+            }
+            return (double)sum / arr.Length;
+        }
+        private int[] GetArrayReplaced(int[] src)
+        {
+            int[] res = new int[src.Length];
+            int max = GetArrayMax(src);
+            double mean = GetArrayMean(src);
+            for (int i = 0; i < src.Length; i++)
+            {
+                if (src[i] % 2 == 0 && src[i] < mean) res[i] = max;
+                else res[i] = src[i];
+            }
+            return res;
+        }
+        private int[] GetArrayDiff(int[] a, int[] b)
+        {
+            int[] diff = Array.Empty<int>();
+            for (int i = 0; i < a.Length; i++)
+            {
+                if (a[i] != b[i])
+                {
+                    Array.Resize(ref diff, diff.Length+1);
+                    diff[diff.GetUpperBound(0)] = i;
+                }
+            }
+            return diff;
+        }
+        public Form_Main()
+        {
+            InitializeComponent();
+        }
+        private uint Parse(string s)
+        {
+            uint n = uint.Parse(s);
+            if (n > maxN) throw new OverflowException($"The value must be reasonable, not greater than {maxN}.");
+            return n;
+        }
+        private void Submit()
+        {
+            string errPath = "error.log";
+            try
+            {
+                uint n = Parse(textBox_n.Text);
+                A = GenerateArray((int)n, range);
+                textBox_A.Text = string.Join(" ", A);
+                dataGridView_A.ColumnCount = A.Length;
+                dataGridView_A.RowCount = A.Length == 0 ? 0 : 1;
+
+                for (int i = 0; i < n; i++)
+                {
+                    dataGridView_A.Columns[i].HeaderCell.Value = i.ToString();
+                    dataGridView_A.Columns[i].AutoSizeMode = DataGridViewAutoSizeColumnMode.AllCells;
+                    dataGridView_A[i, 0].Value = A[i];
+                }
+                B = A.Length == 0 ? new int[0] : GetArrayReplaced(A);
+                textBox_B.Text = string.Join(" ", B);
+
+                int[] diff = GetArrayDiff(A, B);
+
+                dataGridView_B.ColumnCount = B.Length;
+                dataGridView_B.RowCount = B.Length == 0 ? 0 : 1;
+                for (int i = 0; i < n; i++)
+                {
+                    dataGridView_B.Columns[i].HeaderCell.Value = i.ToString();
+                    dataGridView_B.Columns[i].AutoSizeMode = DataGridViewAutoSizeColumnMode.AllCells;
+                    dataGridView_B[i, 0].Value = B[i];
+                    dataGridView_B[i, 0].Style.BackColor = Color.White;
+                    if (diff.Contains(i)) dataGridView_B[i, 0].Style.BackColor = Color.LightCyan;
+                }
+                ToggleOut(true);
+                if (File.Exists(errPath)) File.Delete(errPath);
+            }
+            catch(Exception exc)
+            {
+                MessageBox.Show(exc.Message + "\n" + "See the \"error.log\" file for details.");
+                using (StreamWriter sr = new StreamWriter(errPath, false))
+                {
+                    sr.WriteLine("Timestamp: " + DateTime.Now.ToString(CultureInfo.GetCultureInfo("ru-RU")));
+                    sr.WriteLine("Exception type: " + exc.GetType().Name);
+                    sr.WriteLine("Message: " + exc.Message);
+                    sr.WriteLine("Stack trac: " + exc.Message);
+                }
+            }
+        }
+        private void ToggleOut(bool isOn)
+        {
+            groupBox_out.Visible = isOn;
+            groupBox_output.Visible = isOn;
+        }
+        private void Form_Main_Load(object sender, EventArgs e)
+        {
+            (range[0], range[1]) = (-128, 128);
+            dataGridView_A.AllowUserToAddRows = false;
+            dataGridView_B.AllowUserToAddRows = false;
+            ToggleOut(false);
+        }
+
+        private void textBox_n_KeyPress(object sender, KeyPressEventArgs e)
+        {
+            if (e.KeyChar == (char)Keys.Enter) Submit();
+        }
+
+        private void textBox_n_TextChanged(object sender, EventArgs e)
+        {
+            ToggleOut(false);
+            Control ctrl = sender as Control;
+            errorProvider_n.Clear();
+            try
+            {
+                Parse(ctrl.Text);
+                ctrl.BackColor = Color.LightGreen;
+            }
+            catch
+            {
+                ctrl.BackColor = Color.LightPink;
+            }
+        }
+
+        private void textBox_n_Validating(object sender, CancelEventArgs e)
+        {
+            Control ctrl = sender as Control;
+            ctrl.BackColor = Color.White;
+            try
+            {
+                Parse(ctrl.Text);
+                errorProvider_n.Clear();
+            }
+            catch (Exception exc)
+            {
+                errorProvider_n.SetError(ctrl, exc.Message);
+            }
+        }
+
+        private void button_submit_Click(object sender, EventArgs e)
+        {
+            Submit();
+        }
+
+        private void button_save_Click(object sender, EventArgs e)
+        {
+            // Displays a SaveFileDialog so the user can save the results files
+            SaveFileDialog saveFileDialog1 = new SaveFileDialog();
+            saveFileDialog1.Filter = "Text file|*.txt";
+            saveFileDialog1.Title = "Save an Text File";
+            saveFileDialog1.ShowDialog();
+
+            // If the file name is not an empty string open it for saving.
+            if (saveFileDialog1.FileName != "")
+            {
+                StreamWriter sw = new StreamWriter(saveFileDialog1.FileName);
+                sw.WriteLine("Original array: " + string.Join(" ", A));
+                sw.WriteLine("Altered array: " + string.Join(" ", B));
+                sw.Close();
+            }
+        }
+    }
+}
