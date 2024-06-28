eb372e222ae5e2ca6497723e3f48e355800ff860
viktor <sterilework@gmail.com>
2024-06-28 22:48:18 +0300

    feat: add file i/o to console project 12

--- src/Exam_Console/12/Program.cs
+++ src/Exam_Console/12/Program.cs
@@ -3,12 +3,17 @@
  * в противном случае сообщение «М на N не делится».
  */
 
+using System.Globalization;
+
 namespace Console_12
 {
     internal class Program
     {
         static void Main(string[] args)
         {
+            CultureInfo ci = CultureInfo.GetCultureInfo("ru-RU");
+            Console.OutputEncoding = System.Text.Encoding.Unicode;
+
             try
             {
                 Console.Write("Enter integer M: ");
@@ -20,10 +25,52 @@ namespace Console_12
                 if (M % N == 0) Console.WriteLine("Quotient is: " + M / N);
                 else Console.WriteLine("M is not divisble by N.");
 
+                string path = "result.txt";
+                using (StreamWriter sw = new StreamWriter(path))
+                {
+                    string pad = new string('*', 10);
+                    sw.WriteLine(pad + "Result" + pad);
+                    sw.WriteLine("M: " + M);
+                    sw.WriteLine("N: " + N);
+                    sw.WriteLine($"M is{(M % N == 0 ? " " : " not ")}divisible by N.");
+                    if (M % N == 0) sw.WriteLine("Quotient: " + M / N);
+                }
+
+                string[] res = File.ReadAllLines(path);
+                Console.WriteLine("*****Reading from file*****");
+                foreach (string line in res)
+                {
+                    Console.WriteLine($"{line}");
+                }
             }
-            catch (Exception e)
+            catch (Exception exc)
             {
-                Console.WriteLine("Invalid input: " + e.Message);
+                TextWriter oldErr = Console.Error;
+                string path = "error.log";
+                using (StreamWriter sw = new StreamWriter(path))
+                {
+                    string pad = new string('=', 10);
+                    sw.WriteLine(pad + "Error Log" + pad);
+                    Console.SetError(sw);
+                    string[] err =
+                    {
+                        "Exception type: " + exc.GetType().Name,
+                        "Message:" + exc.Message,
+                        "Stack trace: " + exc.StackTrace,
+                    };
+                    foreach (string line in err) Console.Error.WriteLine(line);
+                }
+
+                Console.SetError(oldErr);
+                Console.Error.WriteLine("Invalid input: " + exc.Message);
+                Console.Error.Write("Further details are available in \"error.log\" file. Do you want to open it (type y|n)? ");
+                ConsoleKeyInfo key = Console.ReadKey();
+                if (key.Key == ConsoleKey.Y)
+                {
+                    Console.WriteLine(Environment.NewLine);
+                    string[] errRes = File.ReadAllLines(path);
+                    foreach (string line in errRes) Console.Error.WriteLine(line);
+                }
             }
         }
     }
