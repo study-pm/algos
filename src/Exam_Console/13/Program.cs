fa1dc179b25fd50d2177fa85deb1602ecad85880
viktor <sterilework@gmail.com>
2024-07-01 20:41:46 +0300

    feat: add file input to console project 13

--- src/Exam_Console/13/Program.cs
+++ src/Exam_Console/13/Program.cs
@@ -3,12 +3,18 @@
  * и в четвёртую степень – отрицательные
  */
 
+using System.Globalization;
+using System.IO;
+
 namespace Console_13
 {
     internal class Program
     {
         static void Main(string[] args)
         {
+            CultureInfo ci = CultureInfo.GetCultureInfo("ru-RU");
+            Console.OutputEncoding = System.Text.Encoding.Unicode;
+
             Random random = new Random();
             double[] range = { -100, 100 };
 
@@ -19,11 +25,18 @@ namespace Console_13
 
             Console.WriteLine("{0, 6} \t {1, 15}", "x", "y");
 
+            string path = "result.txt";
+            using FileStream fs = File.Open(path, FileMode.Create);
+            using StreamWriter sw = new StreamWriter(fs);
+            sw.WriteLine("{0, 6} \t {1, 15}", "x", "y");
+
             for (int i = 0; i < numbers.Length; i++)
             {
                 numbers[i] = random.NextDouble() * (range[1] - range[0]) + range[0];
                 processed[i] = Math.Pow(numbers[i], numbers[i] < 0 ? 4 : 2);
-                Console.WriteLine("{0, 6} \t {1, 15}", string.Format("{0:N2}", numbers[i]), string.Format("{0:N2}", processed[i]));
+                string resultFormatted = string.Format("{0, 6} \t {1, 15}", string.Format("{0:N2}", numbers[i]), string.Format("{0:N2}", processed[i]));
+                Console.WriteLine(resultFormatted);
+                sw.WriteLine(resultFormatted);
             }
         }
     }
